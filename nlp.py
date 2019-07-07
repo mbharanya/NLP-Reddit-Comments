5e2c1b870a3aef43ee92eff3b53aad0a297de092
mbharanya <spam@trash.li>
2019-07-07 20:07:08 +0200

    First commit

---
+++ nlp.py
@@ -0,0 +1,71 @@
+import nltk.tokenize as nt
+import nltk
+from functools import reduce
+
+# nltk.download('punkt')
+# nltk.download('averaged_perceptron_tagger')
+
+text="""
+1.  I think it's relevant to ask how she brought it up. Did you ask? Was it just spontaneous on her part? Context matters.
+2.  I think that I'm the only one here who think you reacted completely inappropriately the second time. The appropriate response to that was, "Do you realise how disrespectful that sounded? I give you my best, and I do not appreciate you putting me down like that. If there are ways that I can satisfy you better, this is not the way to tell me." I would not forget her comment, and would ask her more about it later, but what you did was totally uncool. Your reaction screams insecurity, petulance, and jealousy. And it is entirely inappropriate to leave her stranded like that.
+3.  Kicking someone out of a car is really dangerous and not very classy. Abandoning people out of cars at night, in strange neighborhoods, or in the woods/middle of nowhere is downright abusive and negligent. Do you have every right to break up with her? Sure. Try not to be an equal asshole on your way out of a relationship.
+4.  I've been waiting for 30 minutes for some clarification of the context of the situation, and here it is.
+>We were coming back from a party and in the party there were some girls talking about having sex with their professors. And she started fondly mentioning him and talked about that he was older more experienced and "the best she has ever had." That does not sound like her saying it to me has no purpose other than to hurt me.
+It sounds like she was shooting the shit with her friends, made a comment that made you realize she didn't regard you as the All-Father of Sex in possession of the Cock to end all Cocks, and you flipped out at this realization, and ended up "pulling over the car and [telling] her to get out. That if the other guy was better than [you] than[sic] he [could] go pick her up." You acted like an immature child. Perhaps her remark was undiplomatic, but your response was far out of proportion and betrayed a petulance and thin skin. You're not going to be the best every girl you've slept with has ever had. Deal with it like an adult. She didn't even phrase it as "a guy that was better than me" as you originally claimed in the OP. Ugh.
+5.  You didn't happen to ask about it did you? If you pushed the issue then she made a mistake of being too candid and a "white lie" would have been appropriate, but the main blame would fall on you. If she gave you that opinion unsolicited she's a bitch.
+I'm not saying to dump her because I don't know your situation, but if a girl said that to me she'd be gone in an instant. I would feel disrespected and also, I wouldn't be able to have sex with her without thinking about it.
+6.  Just curious, how did this get brought up in conversation?
+7.  Really, the most basic, fundamental question is: on a scale of 1-10, how drunk was she?
+8.  I don't need to be the best sex a girl has ever had, but at the same time she should never tell me I'm not unless I'm foolish enough to ask.
+9.  Why do you care so much about not being a "pussy"? It's not a boxing ring, it's a relationship. Maybe you should actually ask them for advice on how to improve instead of dumping them on the side of the road when the subject of past relationships comes up. Granted, it looks like they weren't very tactful about it, but damn. Insecure much?
+And if communication is a no-go for some reason, you probably should start watching these Nina Hartley videos.
+10.  "Wow, are you serious? That's cool. Did you know my next girlfriend is a way better girlfriend than you? Later."
+11.  There's a lot of agreement with what you did. If the girlfriend says that in an incautious way, it is a red flag. On the other hand, you might be able to turn it into an opportunity to be better at sex. To me, your reaction was a little extreme, although not necessarily wrong.
+12.  That's classless, but you're over-reacting.
+Chicks tend not to realize that by saying some dude had a bigger dick or fucked em better and made them come EVERY SINGLE TIME that that's like kicking you in the nads.
+13.  Thought this was about overall better guy, was going to commend you for your second response. Realizing it's about sex, you sound like a total tool. Were you guys on the subject? Because that is something weird to just come up in conversation out of nowhere twice, I'm assuming you asked. Your response should be to ask what she fucking likes in bed and work towards her needs. Your ego won't let you see that everyone is different and has different needs, if you're not meeting them you find out what they like not act like a jackass because they don't think you're some mystical sex god.
+14.  I'm not sure i'd dump the girl ON THE SIDE OF THE ROAD. I would take her home and not speak to her again.
+15.  Wow you're a douchebag. What the fuck dude. That's like if you told a girl "my last girlfriend had a bigger ass than you and I really would love if you had a slightly bigger ass" or something and she threw you out of the car for it.But instead, because she cares about you a bit and is not afraid to admit "my ass is a little flat I wouldn't mind building it a tad" and then joining a gym and doing squats or something.
+So instead of learning how to be better at sex or a new thing to twist at her, you decided "no I will not improve! I will not admit the true problem! I AM THE BEST AT SEX THERE IS! DO NOT DOUBT MY COCK!" Seriously. You're an ass. And it's not like this wasn't the first time it happened to you. Nope. You're infallible apparently. Instead of realizing a pattern here of "my ex boyfriend was better at shafting me than you are" meant maybe you have a thing or two to learn, you just took it as "Telling me I have anything to learn in a subject I have obvious insecurities in is disrespectful and I will have none of that bouncing on my dick!" You have a lot of growing up to do. Shit, dude. I'm upvoting the thread in hopes that somebody in your position reads the post and realizes they've done the same thing. Seriously. It might be time to invest in a sex coach. Or at least a book.
+16.  Okay, so your gf seemed to have let it slip that there was someone better in bed then you.
+You handled the situation very poorly. You kicked her out of your car and onto the road in the middle of the night after a party. The above is the kicker for me, any other reaction would have been better then that, but all it does is make you look insecure.
+I would start by apologizing, a lot and trying to open up your guys communication about sex. It sounds like your already committed to being a better lover, so all you need to do is ask what she would want, and try to improve together. But don't expect things to just magically go back to normal. You kicked her out of your car in the middle of the night over something most would consider minor. Yeah, I wouldn't say it was smart, or even in good taste to compare you to a previous lover, which is obviously what sent you to fly off the handle in the first place, but you need to be more composed then that. Fighting fire with more fire isn't going to help anyone. Telling your gf that you didn't appreciate being compared to an old flame of her's would have been good, going as far as to explain to her how it made you feel like an inadequate lover, or second best would have been even better. Throwing someone out of a car is going to accomplish one thing and one thing only: Making it much worse. Communication is key, calmly approach her and explain why you felt the way you did. Apologize for reacting the way you did, as it was an overreaction. Tell her you will not do it again, but that you would also appreciate her not comparing you to old ex's (refer to above for reasons why). Then suggest being more open about your sex life so you both can experiment and get even better together.
+17.  If you asked about her past partners, then you had it coming. If she offered up the information, that's really inconsiderate. Bringing up the past like that is rude.
+18.  Why did you even ask the question? You don't give a shit about any answer that doesn't agree with the one you already have cemented in your head.
+19.  That's a pretty bullshit statement. I feel a good partner should bring out the best in you, and vice versa, but not by telling you "Oh so and so was better than you at this." If I was with someone for some time and they never showed any emotions around me and it was something that upset me I would first implement ways of trying to get them to open up. If it wasn't working then I would broach the subject as "Look, I really care about you, but I'm struggling to create an emotional connection. This is something that is important to me." If it just wasn't going to work after that then so be it, but I wouldn't just tell a girlfriend "My ex-girlfriend was just so much more emotionally available and attractive to me than you are." Her defense of "I should be able to tell you anything" is pretty weak. Sure, she should be able to share anything with you, but not in an attacking/abusive manner like she did. Now is there a chance that she did try to take a more tactful approach and you weren't listening? Perhaps she felt the need to stoop to that tactic because she has noticed making you jealous is the only way to get you to pay attention, so I challenge you reflect upon your time together and see if this could have been the case. Maybe you need to work on your communication skills.
+20.  Who the fuck puts someone who trusts them out on the side of the road, you guys are fucking sociopaths, since when did I subscribe to theredpill.
+21.  It depends on the context, but you sound like you're overreacting. If she casually mentions something about how he was better at something in bed, it could just be a hint that she wants to do something new with you. There's no flipping out necessary. If she's constantly criticizing you and comparing you in a negative way, then you should end things.
+22.  If it was in a way where she helped you in improve in bed like someone did a certain thing or thrust a certain way and showed you how than I wouldn't take a offense to it. But if they were lording it over you than they are being a cunt.
+23.  >How do you handle when a girlfriend/wife brings up that there was a better guy in the past?
+I bring up all the better girls.
+24.  quick note If you have never been in a relationship and/or are usually pussy whipped please don't be handing out advice. I had one guy call me an idiot/asshole for what I did, and he had posts in /r/foreveralone please no white knight. And if you don't like my method of handling it suggest a better way to solve it.
+Hahahahaha, that's /r/Askmen in a nutshell.
+25.  Do you need to up your game in bed? This might be your wakeup call. Or maybe she's just fucking with your head.
+26.  Wait, I think it'd be one thing if she said 'you'll never be like him' and another she reluctantly admitted that a previous partner was better after you pried for answers.
+But it isn't like the two of you couldn't establish the best sex-life the two of you ever had. 
+Someone else having been better, and she being dissatisfied with you aren't the same thing.
+27.  I'm not sure i'd dump the girl ON THE SIDE OF THE ROAD. I would take her home and not speak to her again.
+28.  The people in this thread are fucked up. A girlfriend mentions that she has had better sex in her past, and y'all are fine with leaving her stranded? That's so incredibly immature. I feel like thus thread is being overrun by teenagers. I seriously can't believe so many people in here are advocating breaking up with someone if they admit they have had better sex before. It's crazy.
+29.  That's a pretty bullshit statement. I feel a good partner should bring out the best in you, and vice versa, but not by telling you "Oh so and so was better than you at this." If I was with someone for some time and they never showed any emotions around me and it was something that upset me I would first implement ways of trying to get them to open up. If it wasn't working then I would broach the subject as "Look, I really care about you, but I'm struggling to create an emotional connection. This is something that is important to me." If it just wasn't going to work after that then so be it, but I wouldn't just tell a girlfriend "My ex-girlfriend was just so much more emotionally available and attractive to me than you are." Her defense of "I should be able to tell you anything" is pretty weak. Sure, she should be able to share anything with you, but not in an attacking/abusive manner like she did. Now is there a chance that she did try to take a more tactful approach and you weren't listening? Perhaps she felt the need to stoop to that tactic because she has noticed making you jealous is the only way to get you to pay attention, so I challenge you reflect upon your time together and see if this could have been the case. Maybe you need to work on your communication skills.
+30.  I smiled when I read you told her to get out. Huge props for that and fuck her dipshit logic about being able to share that crap because she's your girlfriend.
+"""
+ss=nt.sent_tokenize(text)
+tokenized_sent=[nt.word_tokenize(sent) for sent in ss]
+pos_sentences=[nltk.pos_tag(sent) for sent in tokenized_sent]
+
+pronouns = list(filter(lambda tuple: tuple[1] in ["PRP", "POS", "PRP$"], reduce(list.__add__, pos_sentences)))
+# print(reduce(list.__add__, pos_sentences))
+
+personal = 0
+possessive = 0
+for pronoun in pronouns:
+    pType = pronoun[1]
+    if pType in ["POS", "PRP$"]:
+        possessive += 1
+    elif pType == "PRP":
+        personal +=1
+        
+
+print(pronouns)
+print("Personal " + str(personal))
+print("Possesive " + str(possessive))
